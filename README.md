68672129d36c9e8d488ae55edce1cc23200ab20a
CarelessOutcast <tapiacarlos32@gmail.com>
2023-07-04 13:16:39 -0500

    Add Readme; Add main file w/comments

---
+++ README.md
@@ -0,0 +1,31 @@
+# Simple Script to start a c++ Project
+
+TODO
+- Add simple Cmake instead of make 
+
+## Motivation
+
+I want to get a standard way of creating a c++ project; To make it faster for me
+build rather than to set up the environment. I want something similar to an IDE
+but I also want to learn how to build something from the ground up and manage it
+myself; Henceforth, this small project was created.
+
+## Usage
+
+Build the binary, that creates a generic project template
+``` 
+make create
+```
+
+Once built, 
+```
+./create ProjectName
+```
+or add to the PATH and have access to it on the system!
+
+```
+create ProjectName
+```
+
+
+
---
+++ create.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <filesystem>
+
+
+
+int main (int argc, char *argv[]) {
+  // First make the directory; 
+  // - ProjectName/
+  
+  // Create the files
+  // - ~/ProjectName/MakeFile
+  // - ~/ProjectName/projectName.cpp
+  // - ~/ProjectName/testFile.txt
+  // - ~/ProjectName/README.md
+
+  
+  // fill the files with templated info; 
+  
+  /* MakeFile 
+   * 
+   *
+   *
+   * */
+
+  /* projectName.cpp 
+   * #include <iostream>
+   * class 
+   * int main(int argc, char* argv[]){
+   *  
+   *
+   * return 0;
+   * }
+   *
+   * */
+
+  /* README.md 
+   * ProjectName: 
+   * Author: Carlos Tapia
+   * StartDate:
+   * Goal: 
+   * Description: 
+   * */
+  return 0;
+}
